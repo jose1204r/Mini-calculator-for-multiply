979ae9476e5893a7b848018953f8bce7091950f1
jose <jrodriguez@itsolutionspcllc.com>
2022-10-08 18:35:22 -0400

    Mini multyply

---
+++ mothod/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace mothod
+{
+
+    
+
+
+    }
+
+    internal class program
+    {
+
+
+    public static int numbers (int numb1, int numb2)
+    
+    {
+
+        var sumnums = numb1 * numb2;
+
+
+        return sumnums;
+        
+    }
+
+    
+
+
+        static void Main(string [] args)
+        {
+
+
+
+        Console.WriteLine("Place your numer to multiply :");
+
+        int userImput1 = Convert.ToInt32(Console.ReadLine());
+
+        int userimput2 = Convert.ToInt32(Console.ReadLine());
+
+        var result = numbers(userImput1, userimput2);
+
+
+        Console.WriteLine($" The Result : {result}");
+        Console.ReadLine();
+
+
+
+        }
+
+    }
+
